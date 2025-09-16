3331d7c99a1cd39252ce89d1fde37543db868e1b
Adon Metcalfe <adon.metcalfe@dpc.wa.gov.au>
2025-09-16 19:10:30 +0800

    Release v1.4.5

--- src/wagov_squ/__init__.py
+++ src/wagov_squ/__init__.py
@@ -1,6 +1,6 @@
 """nbdev-squ - Python SIEM Query Utils with Ibis support."""
 
-__version__ = "1.4.4"
+__version__ = "1.4.5"
 
 # Import main API components
 from .api import Fmt, clients, list_securityinsights, list_workspaces, query_all
